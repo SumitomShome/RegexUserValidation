d17efb6088d0a8ba80661bfe134892edccad3fb3
SumitomShome <sumitomshome@gmail.com>
2020-10-06 13:45:21 +0530

    [Sumitom] Refactor: Commiting UC3-EmailValidation

--- RegexUserValidation/RegexValidation.cs
+++ RegexUserValidation/RegexValidation.cs
@@ -7,11 +7,25 @@ namespace RegexUserValidation
     public class RegexValidation
     {
         public static string FIRST_NAME_REGEX = "^[A-Z]{1}[A-Za-z]{3,}$";
+        public static string LAST_NAME_REGEX = "^[A-Z]{1}[A-Za-z]{3,}$";
+        public static string EMAIL_REGEX = "^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*[@][0-9a-zA-Z]+.[a-zA-Z]{2,3}(.[a-zA-z]{2})?$";
         public bool ValidateFirstName()
         {
             Console.WriteLine("Enter first name: ");
             string firstName = Console.ReadLine();
             return Regex.IsMatch(firstName, FIRST_NAME_REGEX);
         }
-     }
+        public bool ValidateLastName()
+        {
+            Console.WriteLine("Enter last name: ");
+            string lastName = Console.ReadLine();
+            return Regex.IsMatch(lastName, LAST_NAME_REGEX);
+        }
+        public bool ValidateEmail()
+        {
+            Console.WriteLine("Enter email: ");
+            string email = Console.ReadLine();
+            return Regex.IsMatch(email, EMAIL_REGEX);
+        }
+    }
 }
