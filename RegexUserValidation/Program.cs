80c261c416d05817366c9d1ebf47cbf75a821286
SumitomShome <sumitomshome@gmail.com>
2020-10-06 13:57:14 +0530

    [Sumitom] Refactor: Commiting UC2-LastNameValidation

---
+++ RegexUserValidation/Program.cs
@@ -0,0 +1,16 @@
+using System;
+namespace RegexUserValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegexValidation regex = new RegexValidation();
+            bool validResult = regex.ValidateFirstName();
+            bool validLastNameResult = regex.ValidateLastName();
+            Console.WriteLine("First name: " + validResult);
+            Console.WriteLine("Last name: " + validLastNameResult);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RegexUserValidation/RegexValidation.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace RegexUserValidation
+{
+    public class RegexValidation
+    {
+        public static string FIRST_NAME_REGEX = "^[A-Z]{1}[A-Za-z]{3,}$";
+        public static string LAST_NAME_REGEX = "^[A-Z]{1}[A-Za-z]{3,}$";
+        public bool ValidateFirstName()
+        {
+            Console.WriteLine("Enter first name: ");
+            string firstName = Console.ReadLine();
+            return Regex.IsMatch(firstName, FIRST_NAME_REGEX);
+        }
+        public bool ValidateLastName()
+        {
+            Console.WriteLine("Enter last name: ");
+            string lastName = Console.ReadLine();
+            return Regex.IsMatch(lastName, LAST_NAME_REGEX);
+        }
+    }
+}
